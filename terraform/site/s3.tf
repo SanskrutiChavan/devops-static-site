486259e84aaa02ee49eb88153740ffeac6dde253
SANSKRUTI CHAVAN <115650765+SanskrutiChavan@users.noreply.github.com>
2025-12-19 12:09:56 +0530

    Update s3.tf

--- terraform/site/s3.tf
+++ terraform/site/s3.tf
@@ -16,3 +16,32 @@ resource "aws_s3_bucket_website_configuration" "site" {
     suffix = "index.html"
   }
 }
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    sid    = "AllowCloudFrontRead"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = ["s3:GetObject"]
+
+    resources = [
+      "${aws_s3_bucket.site.arn}/*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.cdn.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "site_policy" {
+  bucket = aws_s3_bucket.site.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
