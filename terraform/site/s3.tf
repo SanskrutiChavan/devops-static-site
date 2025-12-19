f6e344611cba7f607c9a520ca7d5e939a69435e7
SANSKRUTI CHAVAN <115650765+SanskrutiChavan@users.noreply.github.com>
2025-12-19 14:21:23 +0530

    Update s3.tf

--- terraform/site/s3.tf
+++ terraform/site/s3.tf
@@ -5,8 +5,10 @@ resource "aws_s3_bucket" "site" {
 resource "aws_s3_bucket_public_access_block" "public" {
   bucket = aws_s3_bucket.site.id
 
-  block_public_acls   = false
-  block_public_policy = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_website_configuration" "site" {
