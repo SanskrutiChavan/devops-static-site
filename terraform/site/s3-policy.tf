83d280fbbce6b0bc175171cd74d57954599982f7
SANSKRUTI CHAVAN <115650765+SanskrutiChavan@users.noreply.github.com>
2025-12-19 12:43:58 +0530

    Update s3-policy.tf

--- terraform/site/s3-policy.tf
+++ terraform/site/s3-policy.tf
@@ -5,18 +5,11 @@ resource "aws_s3_bucket_policy" "site_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid = "AllowCloudFrontRead"
-        Effect = "Allow"
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        }
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.site.arn}/*"
-        Condition = {
-          StringEquals = {
-            "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn
-          }
-        }
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.site.arn}/*"
       }
     ]
   })
