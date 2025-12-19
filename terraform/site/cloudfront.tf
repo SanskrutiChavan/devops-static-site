b3234d677c67429f30e0507c8e2eb1862b5a1a09
SANSKRUTI CHAVAN <115650765+SanskrutiChavan@users.noreply.github.com>
2025-12-19 11:48:31 +0530

    Update cloudfront.tf

--- terraform/site/cloudfront.tf
+++ terraform/site/cloudfront.tf
@@ -17,4 +17,10 @@ resource "aws_cloudfront_distribution" "cdn" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
 }
